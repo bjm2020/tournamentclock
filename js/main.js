2e28c61584153c82da1043b0a3779a49b4de2732
bjm2020 <bjm2020@gmail.com>
2017-06-09 20:54:57 -0500

    Progress Made still some bugs. Test UI Started

--- js/main.js
+++ js/main.js
@@ -8,45 +8,84 @@ var tournamentData = {
 var levels = [
   {smallBlind: 25,
    bigBlind: 50,
+   level: 1,
  breakAfter: false},
  {smallBlind: 50,
   bigBlind: 100,
+  level: 2,
 breakAfter: false},
 {smallBlind: 75,
  bigBlind: 150,
+ level: 3,
 breakAfter: false},
 {smallBlind: 100,
  bigBlind: 200,
+ level: 4,
 breakAfter: true},
 {smallBlind: 10000,
  bigBlind: 20000,
+ level: 5,
 breakAfter: false}];
 
-var Tournament = function(data) {
+this.currentLevel = 0;
+this.nextLevel = 1;
+
+var Tournament = function(data,levelArray) {
   this.tournamentType = data.tournamentType;
-  this.currentSmallBlind = ko.observable();
-  this.currentBigBlind = ko.observable();
   this.currentAnte = ko.observable();
-  this.nextSmallBlind = ko.observable();
-  this.nextBigBlind = ko.observable();
   this.nextAnte = ko.observable();
   this.currentRound = data.currentRound;
   this.prizePool = data.prizePool;
   this.playersLeft = data.players;
   this.startingStack = data.startingStack;
-  this.currentLevel = 0;
-  this.nextLevel = 1;
-  this.chipAverage;
-  this.time;
+  //console.log(levelArray);
+  this.levels = ko.observableArray(levelArray);
+  console.log(this.levels[0]);
+//  this.currentLevel = 0;
+  //console.log(this.currentLevel);
+//  this.nextLevel = 1;
+  this.averageStack = " ";
+  this.time = " ";
+
+  this.currentSmallBlind = ko.computed(function() {
+    console.log(this.levels[0].smallBlind);
+    return this.levels[this.currentLevel].smallBlind;
+  });
+
+  this.currentBigBlind = ko.computed(function() {
+    return this.levels[this.currentLevel].bigBlind;
+  });
+
+  this.nextSmallBlind = ko.computed(function() {
+    return this.levels[this.nextLevel].smallBlind;
+  });
+
+  this.nextBigBlind = ko.computed(function() {
+    return this.levels[this.nextLevel].bigBlind;
+  });
+
+  this.blinds = ko.computed(function(){
+    return this.currentSmallBlind + "/" + this.currentBigBlind;
+  });
+
+  this.nextBlinds = ko.computed(function() {
+    return this.nextSmallBlind + "/" + this.nextBigBlind;
+  });
 }
+
+Tournament.prototype.setLevels = function(levelArray) {
+  levelArray.forEach(function(level) {
+    self.levels.push(new Level(level));
+  });
+};
 
 Tournament.prototype.increaseBlinds = function(levels) {
   this.currentLevel++;
   this.nextLevel++;
-  this.currentSmallBlind(levels[this.currentLevel].smallBlind);
-  this.currentBigBlind(levels[this.currentLevel].bigBlind);
-  this.nextSmallBlind(levels[this.currentLevel+1].smallBlind);
-  this.nextBigBlind(levels[this.currentLevel+1].bigBlind);
+  this.currentSmallBlind(self.levels[this.currentLevel].smallBlind);
+  this.currentBigBlind(self.levels[this.currentLevel].bigBlind);
+  this.nextSmallBlind(self.levels[this.currentLevel+1].smallBlind);
+  this.nextBigBlind(self.levels[this.currentLevel+1].bigBlind);
 };
 
 Tournament.prototype.pauseClock = function() {
@@ -77,8 +116,8 @@ Tournament.prototype.initiateClock = function(duration, display) {
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    display.textContent =  minutes + ":" + seconds;
-    //this.time = minutes + ":" + seconds;
+    //display.textContent =  minutes + ":" + seconds;
+    this.time = minutes + ":" + seconds;
 
     if (--timer < 0) {
       timer = duration;
@@ -128,12 +167,26 @@ var Level = function(data) {
   this.round = data.round;
   this.breakAfter = data.breakAfter;
 }
-
-function startTournament() {
+function viewModel() {
   var self = this;
+    startTournament();
+
+
+  function startTournament() {
+    var self = this;
+    var tournament = new Tournament(tournamentData, levels);
+  //  tournament.setLevels(levels);
+    self.currentTournament = ko.observable(tournament);
+    console.log(currentTournament());
+  }
 
 
 }
+var model = new viewModel();
+ko.applyBindings(model);
+
+//startTournament();
+
 
 function endTournament() {
 
